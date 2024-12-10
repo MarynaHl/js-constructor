0d2c5ba63ab3afca74878df186fe36cc98265fb3
hlushak <75501308+MarynaHl@users.noreply.github.com>
2024-12-10 21:43:14 +0000

    upd

---
+++ index.js
@@ -0,0 +1,12 @@
+const model = [
+    { type: 'title', value: 'Hello World from JS' },
+    { type: 'text', value: 'here some text' },
+    {
+        type: 'columns', value: [
+            '11111111',
+            '22222222',
+            '33333333'
+    ] }
+]
+
+const $site = document.querySelector(selectors: '#site')
